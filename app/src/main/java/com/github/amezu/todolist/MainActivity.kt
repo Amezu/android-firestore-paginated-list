299698c74ea6458614b21f1e1a9af492e3a4ff51
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2020-12-08 18:06:05 +0100

    Init repo

---
+++ app/src/main/java/com/github/amezu/todolist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.amezu.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.amezu.todolist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/github/amezu/todolist/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.amezu.todolist.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
