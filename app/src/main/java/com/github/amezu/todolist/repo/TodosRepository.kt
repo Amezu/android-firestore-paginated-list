a84f86e018e28d46e958a316409f6f012a156989
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2020-12-09 19:47:35 +0100

    Add Firestore to project

--- app/src/main/java/com/github/amezu/todolist/repo/TodosRepository.kt
+++ app/src/main/java/com/github/amezu/todolist/repo/TodosRepository.kt
@@ -2,17 +2,23 @@ package com.github.amezu.todolist.repo
 
 import android.util.Log
 import com.github.amezu.todolist.model.Todo
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObjects
+import com.google.firebase.ktx.Firebase
 import io.reactivex.Single
 
 class TodosRepository {
+    private val db = Firebase.firestore
+    private val collection = db.collection("todos")
+
     fun get(): Single<List<Todo>> {
-        return Single.fromCallable {
-            Log.d("MainViewModel", "single")
-            listOf(
-                Todo("title 1", "description"),
-                Todo("title 2", "description"),
-                Todo("title 3", "description")
-            )
+        return Single.create<List<Todo>> { emitter ->
+            collection.get()
+                .addOnSuccessListener { querySnapshot ->
+                    emitter.onSuccess(querySnapshot.toObjects())
+                }.addOnFailureListener { error ->
+                    emitter.onError(error)
+                }
         }
     }
 }
