5726041fcd4bc792fe8ed8ecd19b16e5e7d2f3ab
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2020-12-14 00:14:21 +0100

    Refactor TodoFormViewModel

--- app/src/main/java/com/github/amezu/todolist/ui/form/TodoFormViewModel.kt
+++ app/src/main/java/com/github/amezu/todolist/ui/form/TodoFormViewModel.kt
@@ -1,10 +1,9 @@
 package com.github.amezu.todolist.ui.form
 
-import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.github.amezu.todolist.R
+import com.github.amezu.todolist.data.FormValidator
 import com.github.amezu.todolist.data.model.Todo
 import com.github.amezu.todolist.data.model.form.TodoFormState
 import com.github.amezu.todolist.data.repo.TodosRepository
@@ -27,7 +26,7 @@ class TodoFormViewModel @Inject constructor(
     val result: LiveData<Throwable?> = _result
 
     fun doOnSaveClick(title: String, description: String, iconUrl: String) {
-        validate(title, description, iconUrl)
+        updateFormState(title, description, iconUrl)
         if (formState.value?.isDataValid != true) return
 
         todosRepository.run {
@@ -35,43 +34,14 @@ class TodoFormViewModel @Inject constructor(
                 ?: create(Todo(title, description, iconUrl))
         }.subscribeOn(Schedulers.io())
             .subscribe(
-            { _result.value = null },
-            { _result.value = it }
-        ).addTo(disposables)
+                { _result.value = null },
+                { _result.value = it }
+            ).addTo(disposables)
     }
 
-    private fun validate(title: String, description: String, iconUrl: String) {
-        _formState.value = TodoFormState(
-            validateTitle(title),
-            validateDescription(description),
-            validateIconUrl(iconUrl)
-        )
+    private fun updateFormState(title: String, description: String, iconUrl: String) {
+        _formState.value = FormValidator.validate(title, description, iconUrl)
     }
-
-    private fun validateTitle(title: String): Int? {
-        return when {
-            title.isEmpty() -> R.string.invalid_title_empty
-            title.length > 30 -> R.string.invalid_title_too_long
-            else -> null
-        }
-    }
-
-    private fun validateDescription(description: String): Int? {
-        return when {
-            description.length > 200 -> R.string.invalid_description_too_long
-            else -> null
-        }
-    }
-
-    private fun validateIconUrl(iconUrl: String): Int? {
-        return when {
-            iconUrl.isEmpty() -> null
-            isNotValidUrl(iconUrl) -> R.string.invalid_url
-            else -> null
-        }
-    }
-
-    private fun isNotValidUrl(iconUrl: String) = !Patterns.WEB_URL.matcher(iconUrl).matches()
 
     override fun onCleared() {
         disposables.dispose()
